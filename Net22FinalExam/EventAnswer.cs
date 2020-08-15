cf848ecbd05008e8a4b885efd157cca5a9ee74e8
mrgocat <mrgocat@gmail.com>
2020-08-15 10:29:28 -0400

    My first commit

---
+++ Net22FinalExam/EventAnswer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net22FinalExam
+{
+    class EventAnswer
+    {
+        static void Main(string[] args)
+        {
+            EventReceiver receiver = new EventReceiver();
+            receiver.Name = "Receiver1";
+
+            EventCreater creater = new EventCreater();
+            creater.MyEvent += receiver.HanderEvent;
+
+            creater.CreateEvent();
+        }
+    }
+
+    public class EventReceiver
+    {
+        public string Name { get; set; }
+
+        public void HanderEvent(object sender, MyEventArgs e)
+        {
+            Console.WriteLine("{0} received Event {1}", Name, e.Info);
+        }
+    }
+
+    public class EventCreater
+    {
+        public event MyEventHandeler MyEvent;
+
+        public void CreateEvent()
+        {
+            Console.WriteLine("Fire!!!!");
+            if (MyEvent != null)
+            {
+                MyEvent(this, new MyEventArgs("ticked!!"));
+            }
+        }
+    }
+
+
+    public delegate void MyEventHandeler(object source, MyEventArgs e);
+
+    public class MyEventArgs : EventArgs
+    {
+        public string Info { get; set; }
+        public MyEventArgs(string info)
+        {
+            this.Info = info;
+
+        }
+    }
+}
---
+++ Net22FinalExam/GenericAnswer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net22FinalExam
+{
+    class GenericAnswer
+    {
+        static void Mainx(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+    }
+    class MyCollection<T> where T : class
+    {
+        private List<T> _collection = new List<T>();
+        public void Add(T item)
+        {
+            _collection.Add(item);
+        }
+        public T Get(int idx)
+        {
+            return _collection[idx];
+        }
+        public IEnumerable<T> GetSortedList()
+        {
+            IEnumerable<T> results1 = _collection.OrderByDescending(n => n);
+            return results1;
+        }
+    }
+}
